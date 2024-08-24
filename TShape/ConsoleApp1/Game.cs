adb19fcef9cb4c9106e1bdf3a6aa0adc844c1ce6
SantiagoSP12 <166035946+SantiagoSP12@users.noreply.github.com>
2024-08-24 00:28:40 -0400

    Posición relativa

--- TShape/ConsoleApp1/Game.cs
+++ TShape/ConsoleApp1/Game.cs
@@ -4,125 +4,19 @@ using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace TShape
 {
     public class Game:GameWindow
     {
-        private readonly float[] vertices =
-        {
-            //top face
-            -1.5f, 3.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             1.5f, 3.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            -1.5f, 3.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-             1.5f, 3.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            //bottom left face
-            -1.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            -0.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            -1.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            -0.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            //bottom right face
-             1.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             0.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             1.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-             0.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            //bottom middle face
-            -0.5f, 0.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             0.5f, 0.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            -0.5f, 0.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-             0.5f, 0.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            //front head
-            -1.5f, 3.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-             1.5f, 3.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            -1.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-             1.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            //front body
-            -0.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-             0.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            -0.5f, 0.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-             0.5f, 0.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            //back head
-            -1.5f, 3.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             1.5f, 3.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            -1.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             1.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            //back body
-            -0.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             0.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            -0.5f, 0.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             0.5f, 0.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            //left side head
-            -1.5f, 3.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            -1.5f, 3.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            -1.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            -1.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            //right side head
-             1.5f, 3.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             1.5f, 3.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-             1.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             1.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            //left side body
-            -0.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            -0.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            -0.5f, 0.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-            -0.5f, 0.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-            //right side head
-             0.5f, 2.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             0.5f, 2.0f,  0.5f,  0.0f, 0.0f, 0.0f,
-             0.5f, 0.0f, -0.5f,  1.0f, 1.0f, 1.0f,
-             0.5f, 0.0f,  0.5f,  0.0f, 0.0f, 0.0f
 
-
-        };
-        uint[] indices = {
-            //top face
-            0, 1, 2,
-            1, 2, 3,
-            //bottom left face
-            4, 5, 6,
-            5, 6, 7,
-            //bottom right face
-            8, 9, 10,
-            9, 10, 11,
-            //bottom middle face
-            12, 13, 14,
-            13, 14, 15,
-            //front head
-            16, 17, 18,
-            17, 18, 19,
-            //front body
-            20, 21, 22,
-            21, 22, 23,
-            //back head
-            24, 25, 26,
-            25, 26, 27,
-            //back body
-            28, 29, 30,
-            29, 30, 31,
-            //left side head
-            32, 33, 34,
-            33, 34, 35,
-            //right side head
-            36, 37, 38,
-            37, 38, 39,
-            //left side body
-            40, 41, 42,
-            41, 42, 43,
-            //right side head
-            44, 45, 46,
-            45, 46, 47
-        };
-
-        int ElementBufferObject;
-        int VertexBufferObject;
-        int VertexArrayObject;
+        private TObject t,c;
         private Shader shader;
         private Camera camara;
 
         float speed = 1.5f;
 
-        Matrix4 model, view, projection;
+        Matrix4 projection;
         Vector2 lastPos;
         bool firstMove = true;
 
@@ -134,37 +28,21 @@ namespace TShape
         protected override void OnLoad()
         {
             base.OnLoad();
-
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             GL.Enable(EnableCap.DepthTest);
 
-            VertexArrayObject = GL.GenVertexArray();
-            GL.BindVertexArray(VertexArrayObject);
-
-            VertexBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-
-            ElementBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
-
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
-            GL.EnableVertexAttribArray(0);
-
-            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
-            GL.EnableVertexAttribArray(1);
+            t = new();
+            c = new();
+            c.scale(0.05f, 0.05f, 0.05f);
 
             shader = new Shader("../../../shader.vert", "../../../shader.frag");
             shader.Use();
-
-            model = view = projection = Matrix4.Identity;
 
             speed = 1.5f;
 
             camara = new Camera(Vector3.UnitZ * 3);
 
-            projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), Size.X/(float)Size.Y, 0.1f, 100.0f);
+            projection = Matrix4.Identity*Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), Size.X/(float)Size.Y, 0.1f, 100.0f);
 
             CursorState = CursorState.Grabbed;
 
@@ -175,13 +53,9 @@ namespace TShape
             base.OnRenderFrame(args);
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            t.draw(shader, camara.GetViewMatrix(), projection);
+            c.draw(shader, camara.GetViewMatrix(), projection);
             shader.Use();
-            shader.SetMatrix4("model",model);
-            shader.SetMatrix4("view",camara.GetViewMatrix());
-            shader.SetMatrix4("projection",projection);
-
-            GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
-
             SwapBuffers();
 
         }
@@ -233,10 +107,11 @@ namespace TShape
             }
             if (KeyboardState.IsKeyDown(Keys.Escape))
             {
-                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-                GL.DeleteBuffer(VertexBufferObject);
-                shader.Dispose();
                 Close();
+            }
+            if (KeyboardState.IsKeyDown(Keys.P))
+            {
+                t.move(camara.Position.X,camara.Position.Y,camara.Position.Z-3.0f);
             }
             const float sensitivity = 0.2f;
 
--- TShape/ConsoleApp1/Program.cs
+++ TShape/ConsoleApp1/Program.cs
@@ -3,7 +3,7 @@ using TShape;
 
 Console.WriteLine("Hello, World!");
 // This line creates a new instance, and wraps the instance in a using statement so it's automatically disposed once we've exited the block.
-using (Game game = new Game(800, 600, "LearnOpenTK"))
+using (Game game = new Game(1200, 800, "LearnOpenTK"))//establecer el tamaño relativo al tamaño de la pantalla
 {
     game.Run();
 }
